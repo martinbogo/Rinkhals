7b3177068b7d03d7019a5724c93835fd3ff5db4c
Martin Bogomolni <martinbogo@gmail.com>
2026-03-27 08:23:14 -0700

    patches: add missing K3SysUi and gkapi firmware patches

--- files/3-rinkhals/opt/rinkhals/patches/gkapi.KS1_2.7.0.7.sh
+++ files/3-rinkhals/opt/rinkhals/patches/gkapi.KS1_2.7.0.7.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 # This script was automatically generated, don't modify it directly
-# Before MD5: 19946492d97226450d9ec6eb63305e67
-# After MD5: 1d3b9cf0f5df130a1cb844ec3c0a57cd
+# Before MD5: 988971db76662cbbd8d3ef07ed4b6385
+# After MD5: 82f7a1077be7654e4edb545db553a1d9
 
 TARGET=$1
 
 MD5=$(md5sum $TARGET | awk '{print $1}')
-if [ "$MD5" = "1d3b9cf0f5df130a1cb844ec3c0a57cd" ]; then
+if [ "$MD5" = "82f7a1077be7654e4edb545db553a1d9" ]; then
     echo $TARGET is already patched, skipping...
     exit 0
 fi
-if [ "$MD5" != "19946492d97226450d9ec6eb63305e67" ]; then
+if [ "$MD5" != "988971db76662cbbd8d3ef07ed4b6385" ]; then
     echo $TARGET hash does not match, skipping patch...
     exit 1
 fi
 
 PATCH_FILE=/tmp/patch-$RANDOM.bin
-echo 'eYs=' | base64 -d > $PATCH_FILE
+echo '7oU=' | base64 -d > $PATCH_FILE
 
-dd if=$PATCH_FILE skip=0 ibs=1 of=$TARGET seek=6666448 obs=1 count=2 conv=notrunc # 0x65b8d0 / 0x66b8d0 > 0x798b
+dd if=$PATCH_FILE skip=0 ibs=1 of=$TARGET seek=6660316 obs=1 count=2 conv=notrunc # 0x65a0dc / 0x66a0dc > 0xee85
 
 rm $PATCH_FILE
